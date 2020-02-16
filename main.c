d8f7689abebc04052e7d42b5106b9af98df232cd
saibharadwaj135 <60809126+saibharadwaj135@users.noreply.github.com>
2020-02-16 17:04:20 +0530

    week1

---
+++ main.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{  float cel,far;
+   printf("enter the temperature in celcius:");
+    scanf("%f",&cel);
+   
+   far=(cel*9/5)+32;
+   printf("the temperature in farenheit:%f",far);
+   
+   printf("enter the temperature in farenheit:");
+   scanf("%f",&far);
+   
+   cel=((far-32)*5/9);
+   
+   printf("the temperature in celsius:%f",cel);
+   
+
+    return 0;
+}
