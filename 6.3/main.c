7f4aaebce65f925612780002de98615d85f47129
saibharadwaj135 <60809126+saibharadwaj135@users.noreply.github.com>
2020-03-04 21:31:56 +0530

    Add files via upload

--- 6.3/main.c
+++ 6.3/main.c
@@ -1,43 +1,25 @@
 //
 //  main.c
-//  practice
+//  week6.1
 //
 //  Created by sai bharadwaj adapa on 04/03/20.
 //  Copyright © 2020 sai bharadwaj adapa. All rights reserved.
-
+//
 
 #include <stdio.h>
 
 int main()
 {
-    int a[100],i,n,min=0,max=0;
-    printf("enter the size of array");
-    
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    int a[100],i,size;
+    printf("enter the size of array:");
+    scanf("%d",&size);
+    for(i=0;i<size;i++)
     {
         scanf("%d",&a[i]);
     }
-    min=a[0];
-    max=a[0];
-    for(i=0;i<n;i++)
+    for(i=0;i<size;i++)
     {
-        if(min>a[i])
-        {
-            min=a[i];
-        }
-        
+        printf("%d ",a[i]);
     }
-    for(i=0;i<n;i++)
-    {
-        if(max<a[i])
-        {
-            max=a[i];
-        }
-    }
-    printf("the minimum value is %d\n",min);
-    printf("the maximum value is %d\n",max);
     
-    
-    return 0;
 }
