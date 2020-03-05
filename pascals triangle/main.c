8652997bdd11ef602061eddcf6b412512d65c9db
saibharadwaj135 <60809126+saibharadwaj135@users.noreply.github.com>
2020-03-05 21:27:19 +0530

    Add files via upload

---
+++ pascals triangle/main.c
@@ -0,0 +1,49 @@
+//
+//  main.c
+//  pascals triangle
+//
+//  Created by sai bharadwaj adapa on 05/03/20.
+//  Copyright © 2020 sai bharadwaj adapa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int fact(int f)
+{
+    if(f==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return f*fact(f-1);
+    }
+}
+
+int main()
+{
+    int n,i,j,k,z;
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=n;j>=i;j--)
+        {
+            printf(" ");
+        }
+        for(k=0;k<=i;k++)
+        {
+            z=fact(i)/(fact(i-k)*fact(k));
+            printf("%d ",z);
+        }
+        printf("\n");
+    }
+    
+    
+    
+    
+    
+  
+}
+    
+
