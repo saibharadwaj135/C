6797a18656b935818f229aaf1ddf9cbbbc7da2e6
saibharadwaj135 <60809126+saibharadwaj135@users.noreply.github.com>
2020-03-14 19:55:01 +0530

    matrices transpose

--- main.c
+++ main.c
@@ -1,43 +1,42 @@
 //
 //  main.c
-//  practice
+//  matrices
 //
-//  Created by sai bharadwaj adapa on 04/03/20.
+//  Created by sai bharadwaj adapa on 14/03/20.
 //  Copyright © 2020 sai bharadwaj adapa. All rights reserved.
-
-
-#include <stdio.h>
-
+//
+#include<stdio.h>
 int main()
 {
-    int a[100],i,n,min=0,max=0;
-    printf("enter the size of array");
-    
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    int a[20][20],m,n,i,j;
+    scanf("%d%d",&m,&n);
+    for(i=0;i<m;i++)
     {
-        scanf("%d",&a[i]);
-    }
-    min=a[0];
-    max=a[0];
-    for(i=0;i<n;i++)
-    {
-        if(min>a[i])
+        for(j=0;j<n;j++)
         {
-            min=a[i];
+            scanf("%d",&a[i][j]);
         }
         
     }
-    for(i=0;i<n;i++)
-    {
-        if(max<a[i])
+    for(i=0;i<m;i++)
+    {     printf("\n");
+        for(j=0;j<n;j++)
         {
-            max=a[i];
+            
+            printf("%d ",a[i][j]);
+            
         }
     }
-    printf("the minimum value is %d\n",min);
-    printf("the maximum value is %d\n",max);
     
-    
-    return 0;
+    for(i=0;i<n;i++)
+        
+    {   printf("\n");
+        for(j=0;j<m;j++)
+        {
+            printf("%d ",a[j][i]);
+        }
+    }
+
 }
+    
+
