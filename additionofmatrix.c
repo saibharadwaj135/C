14cc146effd40a56b1b7bd31baaa46f74efaa744
saibharadwaj135 <60809126+saibharadwaj135@users.noreply.github.com>
2020-03-14 20:10:48 +0530

    matrixaddition

---
+++ additionofmatrix.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    int a[20][20],b[20][20],c[20][20],i,j,m,n;
+    scanf("%d%d",&m,&n);
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    for(i=0;i<m;i++)
+    {  printf("\n");
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",c[i][j]);
+        }
+    }
+    
+
+}
